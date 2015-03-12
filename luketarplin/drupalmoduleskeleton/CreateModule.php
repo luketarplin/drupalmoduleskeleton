c94076faa38523f37c8ba3279bb89133336c027f
Luke Tarplin <luke@email.com>
2015-03-12 16:49:26 +0000

    Remove bn/ dir and the assoc bin command from composer.json, added CreateModule.php

---
+++ luketarplin/drupalmoduleskeleton/CreateModule.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @author Luke Alexander Tarplin <luke.tarplin@gmail.com>
+* Drupal Module Skeleton Create Module Class
+*/
+namespace luketarplin\drupalmoduleskeleton;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateModule
+{
+	public static function run()
+	{
+		echo 'hit';
+	}
+}
