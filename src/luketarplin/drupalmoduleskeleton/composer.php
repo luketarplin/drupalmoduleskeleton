7c28517c0de46ea7ba0f957270380171966ff5d5
Luke Tarplin <luke@email.com>
2015-03-12 09:15:26 +0000

    Initial Commit

---
+++ src/luketarplin/drupalmoduleskeleton/composer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* @author Luke Alexander Tarplin <luke.tarplin@gmail.com>
+* Drupal Module Skeleton Class
+*/
+namespace luketarplin\drupalmoduleskeleton\composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class DrupalModuleSkeletonPlugin implements PluginInterface
+{
+	public function activate(Composer $composer, IOInterface $io)
+    {
+        
+    }
+}
