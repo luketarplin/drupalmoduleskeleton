bf160c3a70619e9ca119090539378408b9a61aea
Luke Tarplin <luke@email.com>
2015-03-13 18:44:38 +0000

    Added require: symfony/yaml to composer.json & added commands to the CreateModule.php file

--- luketarplin/drupalmoduleskeleton/CreateModule.php
+++ luketarplin/drupalmoduleskeleton/CreateModule.php
@@ -5,18 +5,193 @@
 */
 namespace luketarplin\drupalmoduleskeleton;
 
-use Composer\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Composer\Script\Event;
+use Composer\IO\ConsoleIO;
 
-class CreateModule extends Command
+class CreateModule
 {
-	public static function runTask()
-	{
-		var_dump(func_get_args());
+	/**
+	* Composer\IO\ConsoleIO $IO Input / Output Object
+	*/
+	protected $IO;
 	
-		//$class = new self(__CLASS__);
+	protected $shortName;
+	
+	protected $longName;
+	
+	protected $package;
+	
+	protected $description;
+	
+	protected $modulePath 	= './modules';
+	
+	/**
+	* Array $defaults Module Defaults
+	*/
+	protected $defaults   	= array(
+		'type'				=> 'module',
+		'core'				=> '8.x',
+		'dependencies' 		=>	null,
+		'routing' 			=> true,
+	);
+
+	/**
+	* String $logo Composer Plugin Logo
+	*/
+	private static $logo 	= <<<EOT
+    ___    _____   _   _  _____   __     _
+   |# _ \ |# _  | |#| | ||# _ *| /# \   |*|     MODULE
+   |#| |*||#|_| | |#| | ||#|_|_|/#/\ \  |*|     SKELETON
+   |#|_|*||#| \ \ |#|_| ||#|   /#/__\ \ |*|___  BY LUKE TARPLIN
+   |____/ |_|  \_\|_____||_|  /_/    \_\|_____|
+   
+EOT;
+
+	public function __construct(ConsoleIO $IO)
+	{
+		$this->IO = $IO;
+	}
+
+	/**
+	* @param Composer\Script\Event $event
+	* @param $args arguments
+	* Run Task defined in composer.json
+	*/
+	public static function runTask(Event $event, $args = null)
+	{
+		//Get the IO Object
+		$IO 	= $event->getIO();
 		
-		//$class->getIO()->write(__METHOD__);
+		//Get an instance of this class
+		$class 	= new self($IO);
+		$class->outputLogo()
+		      ->getModulesPath()
+		      ->askModuleName()
+			  ->askModulePackage()
+			  ->askModuleDependencies()
+			  ->askModuleDescription()
+			  ->askAddRouting()
+			  ->build();
+		
+		//Debugging Output
+		$reflection	= new \ReflectionClass($class);
+			  
+		foreach($reflection->getProperties() as $properties){
+			if($properties->getName() !== 'IO'){
+				$properties->setAccessible(true);
+				var_dump($properties->getName(),$properties->getValue($class));
+			}
+		}
+		//End of Debugging Output
+	}
+	
+	/**
+	* Output the Logo
+	* @return $this
+	*/
+	public function outputLogo()
+	{
+		//Write the Logo to output
+		$this->IO->write(self::$logo);
+		
+		return $this;
+	}
+	
+	/**
+	* Get the path of the modules directory
+	* @return $this
+	*/
+	public function getModulesPath()
+	{
+		$modulePath 			= $this->IO->ask('Enter path to Drupal 8 (default ./modules): ');
+	
+		if( ! empty($modulePath))
+			$this->modulePath 	= $modulePath;
+			
+		return $this;
+	}
+	
+	/**
+	* Get the module short / long names
+	* @return $this
+	*/
+	public function askModuleName()
+	{
+		$modulePath 	 = $this->modulePath;
+	
+		$this->shortName = $this->IO->askAndValidate('Enter a module short name (eg smtp): ',
+							function($answer) use($modulePath){
+								if(empty($answer))
+									throw new \RuntimeException('Module short name can not be empty!');
+								elseif(is_dir("{$modulePath}/{$answer}"))
+									throw new \RuntimeException('A module with that short code already exists!');
+								return $answer;
+							});
+		$this->longName  = $this->IO->askAndValidate('Enter a module long name (eg SMTP Authentication Support): ',
+							function($answer){
+								if(empty($answer))
+									throw new \RuntimeException('Module short name can not be empty!');
+								return $answer;
+							});
+		return $this;
+	}
+	
+	/**
+	* Get the module package
+	* @return $this
+	*/
+	public function askModulePackage()
+	{
+		$this->package 		= $this->IO->ask('Enter a module package: ');
+		
+		return $this;
+	}	
+	
+	/**
+	* Get the module description
+	* @return $this
+	*/
+	public function askModuleDescription()
+	{
+		$this->description 	= $this->IO->ask('Enter a module description: ');
+		
+		return $this;
+	}
+	
+	/**
+	* Get the module dependencies
+	* @return $this
+	*/
+	public function askModuleDependencies()
+	{
+		$this->defaults['dependencies'] 	= $this->IO->ask('Enter a list of module dependencies (eg rest,serialization): ');
+		
+		return $this;
+	}
+	
+	/**
+	* Get the module routing preferences
+	* @return $this
+	*/
+	public function askAddRouting()
+	{
+		$this->defaults['routing'] 			= 'Y' === $this->IO->askAndValidate('Do you want a routing.yml file (Y / N): ',
+													  function($answer){
+														if($answer !== 'N' && $answer !== 'Y')
+															throw new \RuntimeException('Please choose Y or N!');
+															
+														return $answer;
+													  });
+		
+		return $this;
+	}
+	
+	/**
+	* Build the module based on options gathered above
+	*/
+	public function build()
+	{
+		//Create a new module directory
+		$created 	= mkdir("{$this->modulePath}/{$this->shortName}", 0700);
 	}
 }
